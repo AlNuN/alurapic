7725e945a91d51265a3f211ac080a97441b73eee
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-04-16 16:46:10 -0300

    Routes, props validation

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,26 @@
 <template>
-  <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-    <input v-on:input="filtro = $event.target.value" type="search" class="filtro" placeholder="filtre por parte do título" />
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro" :key="foto.url">
-        <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
-        </meu-painel>
-      </li>
-    </ul>
-  </div>
+<div class="corpo">
+
+  <meu-menu :rotas="routes" />
+
+  <transition name="page">
+    <router-view></router-view>
+  </transition>
+</div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel.vue';
-import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
+import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
 export default {
   components: {
-    'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva,
+    'meu-menu': Menu,
   },
   data() {
     return {
-      titulo: 'Alurapic',
-      fotos: [],
-      filtro: '',
+      routes,
     }
   },
-  computed: {
-    fotosComFiltro () {
-      if(this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      }
-      return this.fotos;
-    }
-  },
-  created() {
-    this.$http.get('http://localhost:3000/v1/fotos')
-      .then((res) => res.json())
-      .then((fotos) => this.fotos = fotos, (err) => console.log(err));
-  }
 }
 </script>
 
@@ -51,20 +31,12 @@ export default {
     margin: 0 auto;
   }
 
-  .centralizado {
-    text-align: center;
+  .page-enter, .page-leave-active {
+    opacity: 0;
   }
 
-  .lista-fotos {
-    list-style: none;
+  .page-enter-active, .page-leave-active {
+    transition: opacity .5s;
   }
 
-  .lista-fotos-item {
-    display: inline-block;
-  }
-
-  .filtro {
-    display: block;
-    width: 100%;
-  }
 </style>
