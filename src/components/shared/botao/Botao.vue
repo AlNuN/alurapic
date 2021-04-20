bf6429f9c0426f183299fd2665bf8400cccbfe9d
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-04-20 19:51:53 -0300

    Button finished

--- src/components/shared/botao/Botao.vue
+++ src/components/shared/botao/Botao.vue
@@ -1,6 +1,7 @@
 <template>
   <button 
-    class="botao botao-perigo" 
+    class="botao"
+    :class="estiloDoBotao" 
     :type="tipo"
     @click="disparaAcao()"
   >
@@ -10,12 +11,33 @@
 
 <script>
 export default {
-    props: ['tipo', 'rotulo'],
+    props: {
+        tipo: {
+            required: true,
+            type: String,
+        },
+        rotulo: {
+            required: true,
+            type: String,
+        },
+        confirmacao: Boolean,
+        estilo: String,
+    },
     methods: {
         disparaAcao() {
-            if(confirm('Confirma operação?')) {
-                this.$emit('botaoAtivado')
+            if(this.confirmacao){
+                if(confirm('Confirma operação?')) {
+                    this.$emit('botaoAtivado')
+                }
+                return;
             }
+            this.$emit('botaoAtivado')
+        }
+    },
+    computed: {
+        estiloDoBotao() {
+            if(this.estilo == 'padrao' || !this.estilo) return 'botao-padrao';
+            if(this.estilo == 'perigo') return 'botao-perigo';
         }
     }
 }
